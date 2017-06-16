bb4e5447c7a3e91464c02b15e46756308a3017f8
vitaliiborodii <allterraintacticalenforcer@gmail.com>
2017-06-16 14:00:40 +0300

    Implement notifications service

---
+++ src/services/messages-notifcation.js
@@ -0,0 +1,67 @@
+import firebase from '../store/firebase'
+const messaging = firebase.messaging()
+
+/*
+* @TODO - This key shouldn't be in the code
+* It is needed both for server and client, maybe set it into environment variable
+*/
+const FCM_KEY = 'AAAA8D8qjWA:APA91bHKK6zWGVE1PIyt5KRjB13sqEqtFNQMZGdLgu5S3CN-da-gCpVd-PicoN7VoVMP3YZvqEpb0zEQ1UUGQke8V4iVZ9HZ7mZ75uCGNaN3ga1ScfmXWtm9HJTVoz_xstBGjuns5-0E'
+
+/*
+* Ask for user permission to display notifications
+* Get FCM token
+* Subscribe token to the topic (which is company name)
+* @param {String} company
+* @return undefined
+*/
+export const initNotifications = (company) => {
+  messaging.requestPermission()
+  .then(() => messaging.getToken())
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log('currentToken:', currentToken)
+      subscribeTokenToTopic(currentToken, company)
+    } else {
+      console.log('No Instance ID token available. Request permission to generate one.')
+    }
+  })
+  .catch((err) => console.log('Unable to get permission to notify.', err))
+
+  messaging.onMessage((payload) => {
+    console.log('receive online message:', payload) // @TODO - handle it somehow
+  })
+}
+
+const subscribeTokenToTopic = (token, topic) => {
+  return fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `key=${FCM_KEY}`
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      return response.text().then(err => {
+        throw new Error(err)
+      })
+    }
+    console.log(`Subscribed to "${topic}"`)
+  })
+  .catch(error => console.error(error))
+}
+/*
+* @param {String} companyName
+* @param {String} userName
+* @param {String} userIcon
+* @param {String} roomName
+*/
+export const sendNewRoom = (companyName, userName, userIcon, roomName) => {
+  // @TODO - probably want to send data in body
+  // (implement this when server will be ready at Cloud Functions)
+  fetch(`/send?title=${encodeURI('New Room')}&body=${encodeURI(userName + ' has added new room, check it out: "' + roomName + '"')}&icon=${encodeURI(userIcon)}&topic=${encodeURI(companyName)}`, {
+    method: 'POST'
+  })
+  .then(r => r.json())
+  .then(console.log.bind(console))
+  .catch(console.error.bind(console))
+}
